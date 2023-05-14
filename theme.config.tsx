c740ed95a496f4e2873af5b2397aa306cb7176f5
javanance <javanance@gmail.com>
2023-05-14 22:58:49 +0900

    ifrs

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
-    <> 
+    <>
       <svg width="30" height="20" viewBox="0 0 310 310">
         <path
           fill="currentColor"
@@ -14,9 +14,20 @@ const config: DocsThemeConfig = {
           <span style={{ marginLeft: '1rem', fontWeight: 900 }}> GBook</span>
     </>
   ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+      <link rel="stylesheet" href="https://unpkg.com/primeflex/themes/saga-blue.css" />
+      <link rel="stylesheet" href="https://unpkg.com/primeflex/themes/arya-blue.css" />
+      <link rel="stylesheet" href="https://unpkg.com/primeflex/themes/vela-blue.css" />
+
+    </>
+  ),
   chat: {
     // link: 'https://twitter.com/javanance',
-    // icon: <svg  width="24" height="24" fill="currentColor"  viewBox="0 0 16 16"> 
+    // icon: <svg  width="24" height="24" fill="currentColor"  viewBox="0 0 16 16">
     //       <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg>
     link: 'https://discord.com/channels/1106958194179313744/1106958194179313746',
   },
@@ -35,7 +46,7 @@ const config: DocsThemeConfig = {
     labels : 'Leave Feedback',
     // useLink() {
     //       return  'https://github.com/gactuary/gbook/issues/new?title=feedback'
-    // }   
+    // }
   },
   editLink: {
     text: '',
@@ -50,10 +61,10 @@ const config: DocsThemeConfig = {
         if (title === 'About') {
           return <>❓ {title}</>;
         }
-        if (title.startsWith('Ano')) {
-          return <div style= {{ display : 'hidden' }} />;
-        }
-        return <>👉 {title}</>;
+        // if (title.startsWith('Aaa')) {
+        //   return <div style= {{ display : 'hidden' }} />;
+        // }
+        return <>{title}</>;
     },
   },
 }
