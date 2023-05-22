a278670e0281b58d77de1739d8d3d8cbd035f5e6
sun0lee <sun0lee88@gmail.com>
2023-05-23 00:29:43 +0900

    url 상세 + anchor (link)
    1. add anchor <a id="나"></a>
    @ level 2 heading
    
    2. KICS-INDEX 수정 (url 세부 경로)
    3. 가용자본 part
    4. <Steps> 검토

--- components/overlayDemo.tsx
+++ components/overlayDemo.tsx
@@ -7,19 +7,19 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function OverlayDemo(prop) {
-    
+
     // console.log('prop : ' + prop.id);
     const { data, error } = useSWR('/data/KICS-INDEX.json', fetcher);
-    
+
     const op = useRef(null);
 
-    //json file error  or data is null 
+    //json file error  or data is null
     if (error) return <div>Failed to load</div>;
 
     if (!data) {
         return <> {prop.id} </>;
-    }    
-    // no data for prop.id 
+    }
+    // no data for prop.id
     // const aaa = data.filter( s => s.id === prop.id)[0] ;
 
     const aaa = data.find(s => s.id === prop.id);
@@ -27,18 +27,20 @@ export default function OverlayDemo(prop) {
     if(!aaa){
         return <> {prop.id} </>;
     }
-    
+
+
+
     console.log('bbb :' + aaa.name)
 
-    const label = aaa.name + ' ('+ aaa.desc + ')';
+    const label = aaa.title + ' ('+ aaa.desc + ')';
 
     return (
         <>
-            <Link  href = {aaa.url}> 
+              <Link  href = {aaa.url}>
 
                 <Button severity="warning"  link
                     label= {label} style ={{padding : '0px 10px'}}
-                    onMouseEnter={(e) => op.current.toggle(e)} onMouseLeave ={(e) => op.current.toggle(e)} 
+                    onMouseEnter={(e) => op.current.toggle(e)} onMouseLeave ={(e) => op.current.toggle(e)}
                 />
             </Link>
             <OverlayPanel ref={op}>
--- components/prime.tsx
+++ components/prime.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/themes/nova/theme.css";
+// import "./theme.module.scss";
 import "primereact/resources/primereact.min.css";
 import { Button } from 'primereact/button';
 import { Accordion, AccordionTab } from 'primereact/accordion';
@@ -9,6 +10,6 @@ import { Dialog } from 'primereact/dialog';
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { TabView, TabPanel } from 'primereact/tabview';
 import { Panel } from 'primereact/panel';
-
+// import { fieldset } from 'primereact/fieldset';
 
 export { Button, Accordion, AccordionTab, Card, Panel, TabView, TabPanel, OverlayPanel};
